16c3681712fce8d788132410f5f9e04a38b8c997
mominnjann <mominjan2025@gmail.com>
2026-01-06 20:18:26 +0530

    Updated

--- Practice_Questions/printfn.c
+++ Practice_Questions/printfn.c
@@ -2,9 +2,51 @@
 //printf function is used to print some value for example
 // Printing Hello World
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main() {
-    printf("Hello World");  // Output = Hello World
-    return 0;
-}
+// int main() {
+//     printf("Hello World");  // Output = Hello World
+//     return 0;
+// }
+// ___________________________________________________________________________
+
+// //Printing integers
+
+// #include<stdio.h>
+
+// int main() {
+//     int a;
+//     printf("Enter any Integer you want to print ");
+//     scanf("%d", &a);
+//     printf("The Integer in Input is %d \n", a);
+//     return 0;
+// }
+// ____________________________________________________________________________
+
+// Printing Floating Values 
+
+// #include<stdio.h>
+
+// int main() {
+//     float a;
+//     printf("Enter any float value : \n");
+//     scanf("%f", &a);
+//     printf("The Float value given by the user is %.2f \n", a);
+//     return 0;
+// }
+
+//_______________________________________________________________________________
+
+// Printing Characters and Strings
+
+// #include<stdio.h>
+
+// int main() {
+//     char a = 'A';
+//     char b[] = "Momin";
+//     printf( "Name : %s , Grade : %c", b, a);
+
+//     return 0;
+// }
+
+//_____________________________________________________________________________________
