7f0b311bf9ce83aa2ca7aa89bc6cfda4d7df0b37
mominnjann <mominjan2025@gmail.com>
2026-01-06 19:37:58 +0530

    First Commit

---
+++ Practice_Questions/printfn.c
@@ -0,0 +1,10 @@
+
+//printf function is used to print some value for example
+// Printing Hello World
+
+#include<stdio.h>
+
+int main() {
+    printf("Hello World");  // Output = Hello World
+    return 0;
+}
