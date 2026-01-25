1af6704e662ee3c1252269794dc27347668f9d52
mominnjann <mominjan2025@gmail.com>
2026-01-25 08:07:51 +0530

    New Chapter - Strings

---
+++ Practice_Questions/CHAPTER_8_STRINGS.c
@@ -0,0 +1,27 @@
+//                                  CHAPTER 8 – STRINGS
+/*
+CHAPTER 8 – PRACTICE SET 
+1. Which of the following is used to appropriately read a multi-word string. 
+1. gets() 
+2. puts() 
+3. printf() 
+4. scanf() 
+2. Write a program to take string as an input from the user using %c and %s confirm 
+that the strings are equal. 
+3. Write your own version of strlen function from <string.h> 
+4. Write a function slice() to slice a string. It should change the original string such 
+that it is now the sliced string. Take ‘m’ and ‘n’ as the start and ending position 
+for slice. 
+5. Write your own version of strcpy function from <string.h> 
+6. Write a program to encrypt a string by adding 1 to the ascii value of its 
+characters. 
+7. Write a program to decrypt the string encrypted using encrypt function in 
+problem 6. 
+8. Write a program to count the occurrence of a given character in a string. 
+9. Write a program to check whether a given character is present in a string or not. 
+*/
+
+//____________________________________________________________________________________________________________________
+
+//1. Answer
+
