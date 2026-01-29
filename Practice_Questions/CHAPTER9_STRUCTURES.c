453943a2b7464c2ce16424b5718102b8183772ca
mominnjann <mominjan2025@gmail.com>
2026-01-29 23:48:58 +0530

    Updated

--- Practice_Questions/CHAPTER9_STRUCTURES.c
+++ Practice_Questions/CHAPTER9_STRUCTURES.c
@@ -84,7 +84,91 @@ int main() {
 
 //4. Answer
 
+/*
 
+#include<stdio.h>
 
+typedef struct employee{
+    int salary;
+    float score;
+}Employee;
+int main() {
+    Employee e1;
+    Employee* ptr1 = &e1;
+    ptr1->salary = 56;
+    printf("The value of the salary is %d", ptr1->salary);
+    return 0;
+}
 
+*/
 
+//________________________________________________________________________________________________________
+
+//5. Answer
+
+/*
+
+#include<stdio.h>
+
+typedef struct c{
+    int real;
+    int imaginary;
+}Complex;
+int main() {
+    Complex c = {2, 4};
+    printf("The value of complex number is %d + i%d", c.real, c.imaginary);
+    
+    return 0;
+}
+
+*/
+
+//__________________________________________________________________________________________________________
+
+//6.Answer
+
+/*
+
+#include<stdio.h>
+
+typedef struct c{
+    int real;
+    int imaginary;
+}Complex;
+
+void display(Complex c){
+    printf("The value of complex number is %d + i%d\n", c.real, c.imaginary);
+}
+int main() {
+    Complex c[5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter real part\n");
+        scanf("%d", &c[i].real);
+        printf("Enter imagianry part\n");
+        scanf("%d", &c[i].imaginary);
+        display(c[i]);
+    }
+    return 0;
+}
+
+*/
+
+//_____________________________________________________________________________________________________________________
+
+//8. Answer
+
+/*
+#include<stdio.h>
+struct bankacc{
+    int accNo;
+    char name[20];
+    char ifsc[12];
+};
+int main() {
+    
+return 0;
+}
+*/
+
+//_____________________________________________________________________________________________________________________________
